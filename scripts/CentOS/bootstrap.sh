fc15c2e904b0d500989b4d056becc42b7f84bc60
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-09-16 19:15:22 +0200

    working CentOS except sharing file 'vboxsf file system is not available'

--- scripts/CentOS/bootstrap.sh
+++ scripts/CentOS/bootstrap.sh
@@ -1,17 +1,17 @@
 # Falkor Bootstrap to install missing packages etc... 
 
 # Install Ruby
-yum install ruby
+#yum install ruby
 
 # Needed dependencies
-yum install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel
-yum install ruby-rdoc ruby-devel
+#yum install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel
+#yum install ruby-rdoc ruby-devel
 
 # Install Ruby Gems
-yum install rubygems
+#yum install rubygems
 
 # Install librarian-puppet
-gem install librarian-puppet
+# gem install librarian-puppet
 
 
 

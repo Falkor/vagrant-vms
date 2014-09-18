03732291b55bc2c3f2261517f4592ede2523eebb
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-09-18 09:33:15 +0200

    bootstrap.sh save

--- scripts/CentOS/bootstrap.sh
+++ scripts/CentOS/bootstrap.sh
@@ -1,18 +1,30 @@
 # Falkor Bootstrap to install missing packages etc... 
 
-# Install Ruby
-#yum install ruby
-
-# Needed dependencies
-#yum install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel
-#yum install ruby-rdoc ruby-devel
-
-# Install Ruby Gems
-#yum install rubygems
-
-# Install librarian-puppet
-# gem install librarian-puppet
-
+## First remove the old version of ruby (cause librarian-puppet need 1.9.3 or higher and centos6.5 has 1.8.7)
+#yum remove -y ruby ruby-devel
+#
+## Second install ruby 2.1.2 from the source
+#yum -y groupinstall "Development Tools"
+#yum -y install openssl-devel
+#yum -y install wget
+#wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz
+#tar xvfvz ruby-2.1.2.tar.gz
+#cd ruby-2.1.2
+#./configure
+#make
+#make install
+#
+## Third create symlink to have it in the path
+#ln -s /usr/local/bin/* /usr/bin/
+#
+## Forth update Rubygems and Bundler
+#gem update --system
+#gem install bundler
+#
+## Fifth update allready installed gems if any
+#gem update -f
+#
+#yum install -y ruby-devel
 
 
 

882c20a97606ae96fe9bdf00b0a263e5f5cb94d5
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-09-30 15:10:46 +0200

    script puppet fonctionnel pour CentOS 6 (en cours de tests pour CentOS 7)

--- scripts/CentOS/puppet.sh
+++ scripts/CentOS/puppet.sh
@@ -3,7 +3,6 @@
 VERSION=$(awk '{print $3}' /etc/centos-release)
 VERSION=${VERSION:0:1}
 
-
 if [ "$VERSION" -eq "6"]
 then
     wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
@@ -17,7 +16,17 @@ then
 fi
 
 yum install -y libyaml
-rpm -ivh https://www.dropbox.com/s/18z5vvkpnambr1m/ruby-2.1.2-2.el6.x86_64.rpm?dl=0
+# Ancienne methode, ne gere pas les dependances donc problematique
+#rpm -ivh https://www.dropbox.com/s/18z5vvkpnambr1m/ruby-2.1.2-2.el6.x86_64.rpm?dl=0
+
+# Nouvelle methode : utilisation d'un depot maison, de cette facon yum gere les dependances
+echo "[personal-repo-www]" >> /etc/yum.repos.d/personal-repos-www.repo
+echo "gpgcheck=0" >> /etc/yum.repos.d/personal-repos-www.repo
+echo "name=Red Hat Linux $releasever - $basearch" >> /etc/yum.repos.d/personal-repos-www.repo
+echo "baseurl=http://ec2-54-77-188-112.eu-west-1.compute.amazonaws.com/repo/" >> /etc/yum.repos.d/personal-repos-www.repo
+
+yum clean all
+yum install -y ruby
 
 gem install puppet facter ruby-shadow
 

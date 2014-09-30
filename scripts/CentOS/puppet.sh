f523de84df9471d8e16732c309ff893b19b6d3d6
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-09-30 10:33:23 +0200

    retrait de la gestion de CentOS 5

--- scripts/CentOS/puppet.sh
+++ scripts/CentOS/puppet.sh
@@ -1,8 +1,21 @@
 # Install Puppet
 
-wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+VERSION=$(awk '{print $3}' /etc/centos-release)
+VERSION=${VERSION:0:1}
+
+
+if [ "$VERSION" -eq "6"]
+then
+    wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+    wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+    rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+elif [ "$VERSION" -eq "7"]
+then
+    wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
+    wget http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+    rpm -Uvh remi-release-7*.rpm epel-release-7*.rpm
+fi
+
 yum install -y libyaml
 rpm -ivh https://www.dropbox.com/s/18z5vvkpnambr1m/ruby-2.1.2-2.el6.x86_64.rpm?dl=0
 

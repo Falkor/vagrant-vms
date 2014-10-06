8fd5b08d8107349bcda3a0652bd25544e83dccbc
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-10-06 17:14:40 +0200

    ajout du script d'installation d'easybuild (non testé)

--- scripts/core/easybuild.sh
+++ scripts/core/easybuild.sh
@@ -1,3 +1,8 @@
 # Install EasyBuild
 # NB : environment-modules installed in bootstrap.sh
 
+# Si ne marche pas, verifier que python --version >= 2.4
+wget https://raw.github.com/hpcugent/easybuild-framework/develop/easybuild/scripts/bootstrap_eb.py
+python bootstrap_eb.py /usr/bin/.local/easybuild
+echo "MODULEPATH=/usr/bin/.local/easybuild/modules/all:$MODULEPATH" >> /etc/environment
+

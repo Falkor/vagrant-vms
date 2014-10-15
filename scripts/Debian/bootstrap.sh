ead9d7c9e6a63fb9ebdf45c077af76e7b6564df0
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-10-15 17:17:27 +0200

    installation d'EasyBuild totalement fonctionnelle CentOS (Lmod) + Debian (environment-modules)

--- scripts/Debian/bootstrap.sh
+++ scripts/Debian/bootstrap.sh
@@ -6,13 +6,13 @@ apt-get -y install curl figlet vim bash-completion
 # Puppet customization 
 apt-get -y install facter
 
-# Adding testing repository to install Lmod later on (Current version on Debian is to old for EasyBuild, so we use the one from the jessie (testing) repo)
-printf "\n# Testing repository - main, contrib and non-free branches\ndeb http://http.us.debian.org/debian jessie main non-free contrib\ndeb-src http://http.us.debian.org/debian jessie main non-free contrib\n" >> /etc/apt/sources.list
-printf "\n\n# Testing security updates repository\ndeb http://security.debian.org/ jessie/updates main contrib non-free\ndeb-src http://security.debian.org/ jessie/updates main contrib non-free\n" >> /etc/apt/sources.list
+## Adding testing repository to install Lmod later on (Current version on Debian is to old for EasyBuild, so we use the one from the jessie (testing) repo)
+#printf "\n# Testing repository - main, contrib and non-free branches\ndeb http://http.us.debian.org/debian jessie main non-free contrib\ndeb-src http://http.us.debian.org/debian jessie main non-free contrib\n" >> /etc/apt/sources.list
+#printf "\n\n# Testing security updates repository\ndeb http://security.debian.org/ jessie/updates main contrib non-free\ndeb-src http://security.debian.org/ jessie/updates main contrib non-free\n" >> /etc/apt/sources.list
 
 # We set the priorities of the repositories, so that by default apt will install packages from the stable repository
-printf "\nPackage: *\nPin: release a=stable\nPin-Priority: 700\n" >> /etc/apt/preferences.d/priority_preferences
-printf "\n\nPackage: *\nPin: release a=jessie\nPin-Priority: 650\n" >> /etc/apt/preferences.d/priority_preferences
+#printf "\nPackage: *\nPin: release a=stable\nPin-Priority: 700\n" >> /etc/apt/preferences.d/priority_preferences
+#printf "\n\nPackage: *\nPin: release a=jessie\nPin-Priority: 650\n" >> /etc/apt/preferences.d/priority_preferences
 
-apt-get update
+#apt-get update
 
---
+++ scripts/debian-easybuild.sh
@@ -0,0 +1,12 @@
+# Adding testing repository to install Lmod later on (Current version on Debian is to old for EasyBuild, so we use the one from the jessie (testing) repo)
+printf "\n# Testing repository - main, contrib and non-free branches\ndeb http://http.us.debian.org/debian jessie main non-free contrib\ndeb-src http://http.us.debian.org/debian jessie main non-free contrib\n" >> /etc/apt/sources.list
+printf "\n\n# Testing security updates repository\ndeb http://security.debian.org/ jessie/updates main contrib non-free\ndeb-src http://security.debian.org/ jessie/updates main contrib non-free\n" >> /etc/apt/sources.list
+
+# We set the priorities of the repositories, so that by default apt will install packages from the stable repository
+printf "\nPackage: *\nPin: release a=stable\nPin-Priority: 700\n" >> /etc/apt/preferences.d/priority_preferences
+printf "\n\nPackage: *\nPin: release a=jessie\nPin-Priority: 650\n" >> /etc/apt/preferences.d/priority_preferences
+
+apt-get update
+
+puppet apply /etc/puppet/modules/easybuild/tests/init.pp
+

5974291a2f2db6173d7c0d161f35a5030385ef21
Sebastien Varrette <sebastien.varrette@uni.lu>
2015-05-07 18:33:11 +0200

    update Centos scriptqs
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

--- scripts/CentOS/puppet.sh
+++ scripts/CentOS/puppet.sh
@@ -1,31 +1,18 @@
 # Install Puppet
 
-VERSION=$(sed 's/[^0-9]//g' /etc/centos-release)
-VERSION=${VERSION:0:1}
+cat > /etc/yum.repos.d/puppetlabs.repo << EOM
+[puppetlabs-dependencies]
+name=puppetlabdsdependencies
+baseurl=http://yum.puppetlabs.com/el/7/dependencies/\$basearch
+enabled=1
+gpgcheck=0
 
-yum install -y epel-release
+[puppetlabs]
+name=puppetlabs
+baseurl=http://yum.puppetlabs.com/el/7/products/\$basearch
+enabled=1
+gpgcheck=0
+EOM
 
-if [ "$VERSION" -eq "6" ]
-then
-    # Ajout du repository personnel contenant le package ruby
-    echo "[personal-repo-www]" >> /etc/yum.repos.d/personal-repos-www.repo
-    echo "gpgcheck=0" >> /etc/yum.repos.d/personal-repos-www.repo
-    echo "name=Red Hat Linux $releasever - $basearch" >> /etc/yum.repos.d/personal-repos-www.repo
-    echo "baseurl=http://ec2-54-77-95-196.eu-west-1.compute.amazonaws.com/repo/el6/" >> /etc/yum.repos.d/personal-repos-www.repo
-
-    yum clean all
-    yum install -y ruby-2.1.2-2.el6.x86_64
-elif [ "$VERSION" -eq "7" ]
-then
-    # Ajout du repository personnel contenant le package ruby
-    echo "[personal-repo-www]" >> /etc/yum.repos.d/personal-repos-www.repo
-    echo "gpgcheck=0" >> /etc/yum.repos.d/personal-repos-www.repo
-    echo "name=Red Hat Linux $releasever - $basearch" >> /etc/yum.repos.d/personal-repos-www.repo
-    echo "baseurl=http://ec2-54-77-95-196.eu-west-1.compute.amazonaws.com/repo/el7/" >> /etc/yum.repos.d/personal-repos-www.repo
-
-    yum clean all
-    yum install -y ruby-2.1.3-2.el7.centos.x86_64
-fi
-
-gem install puppet facter ruby-shadow
+yum -y install puppet facter ruby-shadow
 
